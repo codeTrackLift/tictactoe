b15b0457a5e82b565421776688245350b8939989
Pete Codes <96446064+codeTrackLift@users.noreply.github.com>
2022-04-22 12:02:44 -0400

    folder name step 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Game } from './Components/Tictactoe';
-import './Styles/Tictactoe.css';
+import { Game } from './components/Tictactoe';
+import './styles/tictactoe.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
